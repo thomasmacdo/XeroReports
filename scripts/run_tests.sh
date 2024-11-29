a652af6041524e1419d85045a702b02ecdc7c4c8
Thomas MacDougall <thomasmacdougall@MacBook-Pro-2.local>
2024-11-29 00:13:41 +0000

    added: database env variables for running tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,14 @@
 find . -type d -name "__pycache__" -exec rm -r {} +
 find . -type f -name "*.pyc" -delete
 
+# Set test database environment variables
+export SQL_ENGINE="django.db.backends.postgresql"
+export SQL_DATABASE="xero_db"
+export SQL_USER="xero_user"
+export SQL_PASSWORD="xero_password"
+export SQL_HOST="localhost"
+export SQL_PORT="5432"
+
 pytest core/tests \
     -v \
     --tb=short \
